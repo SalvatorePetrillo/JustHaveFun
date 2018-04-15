1645bcc67efcef84b47c1468934c20593fc78724
Salvatore Petrillo <s.petrillo96@hotmail.it>
2018-04-15 16:35:01 +0200

    Ho aggiunto i riferimenti alle view in Eventi e InfoEventi. In eventi ho cercato anche di collegare il dataSource e l'adapter (seppur il dataSource non è stato ancora popolato).

--- app/src/main/java/com/example/salvatorepetrillo/justhavefun/Eventi.java
+++ app/src/main/java/com/example/salvatorepetrillo/justhavefun/Eventi.java
@@ -2,12 +2,37 @@ package com.example.salvatorepetrillo.justhavefun;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
+
+import com.example.salvatorepetrillo.justhavefun.datamodel.DataSource;
 
 public class Eventi extends AppCompatActivity {
+
+    //riferimenti alle view
+    private Button vAggiungiEvento;
+    private ListView vListaView;
+
+    //Adapter e DataSource
+    private EventoAdapter adapter;
+    private DataSource dataSource;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_eventi);
+
+        vAggiungiEvento = findViewById(R.id.btnAddEvento);
+        vListaView = findViewById(R.id.ListaEventi);
+
+        //ottengo i riferimenti dal DataSource
+        dataSource = DataSource.getIstance();
+
+        //creo l'adapter
+        //adapter = new EventoAdapter(); // Non cosa passare come secoondo parametro e non sono neanche sicuro che vada fatto qualcosa
+
+        //vListaView.setAdapter(); //logicamente non so quale adapter settare.
+
     }
 }
--- app/src/main/java/com/example/salvatorepetrillo/justhavefun/InfoEvento.java
+++ app/src/main/java/com/example/salvatorepetrillo/justhavefun/InfoEvento.java
@@ -2,12 +2,40 @@ package com.example.salvatorepetrillo.justhavefun;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class InfoEvento extends AppCompatActivity {
+
+    private TextView vNome;
+    private TextView vDescrizione;
+    private Button vPrenota;
+    private Button vTornaAEventi;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info_evento);
+
+        vNome = findViewById(R.id.txtNomeEvento);
+        vDescrizione = findViewById(R.id.txtDescrizioneEvento);
+        vPrenota = findViewById(R.id.btnEffettuaPrenotazione);
+        vTornaAEventi = findViewById(R.id.btnIndietro);
+
+        vPrenota.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        vTornaAEventi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }

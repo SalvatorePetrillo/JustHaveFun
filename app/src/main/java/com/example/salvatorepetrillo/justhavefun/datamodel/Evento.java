e864b3d86da5adddb603711e9e67b70b96e4cdec
DavideBarbato <davide.barbato@studenti.unicampania.it>
2018-04-15 11:01:01 +0200

    Ho creato l'evento

--- app/src/main/java/com/example/salvatorepetrillo/justhavefun/datamodel/Evento.java
+++ app/src/main/java/com/example/salvatorepetrillo/justhavefun/datamodel/Evento.java
@@ -1,6 +1,22 @@
 package com.example.salvatorepetrillo.justhavefun.datamodel;
 
+// classe utile a costruire un evento
 public class Evento {
 
+    //definisco gli attributi
+    private String nomeEvento;
+    private String descrizioneEvento;
 
+    // definisco il costruttore
+    public Evento(String nomeEvento, String descrizioneEvento) {
+        this.nomeEvento = nomeEvento;
+        this.descrizioneEvento = descrizioneEvento;
+    }
+
+    //diamo la possibilità di prendere e settare i nostri attributi
+    public String getNomeEvento (){return nomeEvento;}
+    public void setNomeEvento (String nomeEvento){this.nomeEvento = nomeEvento;}
+
+    public String getDescrizioneEvento (){return descrizioneEvento;}
+    public void setDescrizioneEvento (String descrizioneEvento){this.descrizioneEvento = descrizioneEvento;}
 }

b976114f24f5870c3ab80e3ccd7bc0d223b4e6ee
DavideBarbato <davide.barbato@studenti.unicampania.it>
2018-04-15 17:19:43 +0200

    Ho terminato gli ultimi dettagli e commentato il da farsi per il prosieguo

--- app/src/main/java/com/example/salvatorepetrillo/justhavefun/riga_evento.java
+++ app/src/main/java/com/example/salvatorepetrillo/justhavefun/riga_evento.java
@@ -2,12 +2,16 @@ package com.example.salvatorepetrillo.justhavefun;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class riga_evento extends AppCompatActivity {
 
+    private TextView vRigaEvento;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_riga_evento);
+
+        vRigaEvento = findViewById(R.id.txtNomeCompleto);
     }
 }

4d4a73abb4a3bb527351d7eca3a0eabf85e4268a
Salvatore Petrillo <s.petrillo96@hotmail.it>
2018-04-15 12:04:33 +0200

    Ho quasi ultimato il DataSource e ho creato la layout "riga_evento"

---
+++ app/src/main/java/com/example/salvatorepetrillo/justhavefun/riga_evento.java
@@ -0,0 +1,13 @@
+package com.example.salvatorepetrillo.justhavefun;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class riga_evento extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_riga_evento);
+    }
+}

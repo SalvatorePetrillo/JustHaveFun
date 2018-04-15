85947775ee1e9e591ea66e1e5d4564f9b0c01be3
Salvatore Petrillo <s.petrillo96@hotmail.it>
2018-04-15 11:11:52 +0200

    Sto lavorando con il DataSource...

--- app/src/main/java/com/example/salvatorepetrillo/justhavefun/datamodel/DataSource.java
+++ app/src/main/java/com/example/salvatorepetrillo/justhavefun/datamodel/DataSource.java
@@ -1,5 +1,34 @@
 package com.example.salvatorepetrillo.justhavefun.datamodel;
 
+import java.util.Hashtable;
+
 public class DataSource {
     //abbiamo creato il dataSource
+
+    private Hashtable<String,Evento> elencoEventi;
+
+    private static DataSource istance = null;
+
+    //Costruttore DataSource
+    private DataSource(){
+        elencoEventi = new Hashtable<>();
+        popolaDataSource();
+    }
+
+    //Riferimento ai dati
+    public static DataSource getIstance{
+        if(istance==null)
+            istance = new DataSource();
+        return istance;
+    }
+
+    public void addEvento(Evento evento){
+        elencoEventi.put()
+    }
+
+
+    //Mi servirà per popolare il DataSource
+    private void popolaDataSource(){
+
+    }
 }

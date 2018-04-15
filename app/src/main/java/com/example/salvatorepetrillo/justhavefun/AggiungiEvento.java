96d7820cb99651c2d153a90532b1b49422e95ae6
Salvatore Petrillo <s.petrillo96@hotmail.it>
2018-04-15 12:41:32 +0200

    Ho cominciato a lavorare su "AggiungiEvento"

--- app/src/main/java/com/example/salvatorepetrillo/justhavefun/AggiungiEvento.java
+++ app/src/main/java/com/example/salvatorepetrillo/justhavefun/AggiungiEvento.java
@@ -2,12 +2,30 @@ package com.example.salvatorepetrillo.justhavefun;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class AggiungiEvento extends AppCompatActivity {
+
+    private EditText vInserisciNome;
+    private EditText vInserisciDescrizione;
+    private EditText vInserisciCodice;
+    private Button vConferma;
+    private Button vIndietro;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aggiungi_evento);
+
+        //impostazione dei riferimenti delle view
+        vInserisciNome = findViewById(R.id.editAddNomeEvento);
+        vInserisciDescrizione = findViewById(R.id.editAddDescrizioneEvento);
+        vInserisciCodice = findViewById(R.id.editAddNumeroEvento);
+        vConferma = findViewById(R.id.btnAddConferma);
+        vIndietro = findViewById(R.id.btnTornaIndietro);
+
+
     }
 }
